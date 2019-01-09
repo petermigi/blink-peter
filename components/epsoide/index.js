4c5a19a8669e5ca9b0381531ec645cce6c1243e4
peterliu <3102415881@qq.com>
2019-01-09 14:56:59 +0800

    8-14epsoide组件与流行页面样式

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -19,6 +19,9 @@ Component({
      * 组件的初始数据
      */
     data: {
+        months:[
+            '一月','二月','三月','四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'
+        ],
         year:0,
         month:'',
         _index:''
@@ -26,8 +29,14 @@ Component({
 
     //
     attached:function(){
-        console.log(this.properties)
-        console.log(this.data)
+        let date = new Date()
+        let year = date.getFullYear()
+        let month = date.getMonth()
+
+        this.setData({
+            year:year,
+            month:this.data.months[month]
+        })
     },
 
     /**
